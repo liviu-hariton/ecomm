b35f5e654a17bc2a79f7ca1d4981d3e4034109d3
Liviu Hariton <liviu.hariton@gmail.com>
2023-10-18 19:04:05 +0300

    Admin interface template

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,11 @@
-<h2>Admin dashboard</h2>
+@extends('admin.layouts.master')
+
+@section('main-section')
+    <section class="section">
+        <div class="section-header">
+            <h1>Dashboard</h1>
+        </div>
+
+
+    </section>
+@endsection
