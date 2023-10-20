c45edfe190d4595249cb3545b15e7d2f1fffaa24
Liviu Hariton <liviu.hariton@gmail.com>
2023-10-20 23:47:27 +0300

    Public Frontend & Homepage layout

---
+++ resources/views/frontend/home/home.blade.php
@@ -0,0 +1,29 @@
+@extends('frontend.layouts.master')
+
+@section('main-content')
+
+    @include('frontend.home.sections.top-slider')
+
+    @include('frontend.home.sections.flash-sale')
+
+    @include('frontend.home.sections.top-category-product')
+
+    @include('frontend.home.sections.brand-slider')
+
+    @include('frontend.home.sections.single-banner')
+
+    @include('frontend.home.sections.hot-deals')
+
+    @include('frontend.home.sections.product-slider-one')
+
+    @include('frontend.home.sections.product-slider-two')
+
+    @include('frontend.home.sections.large-banner')
+
+    @include('frontend.home.sections.best-rated')
+
+    @include('frontend.home.sections.services')
+
+    @include('frontend.home.sections.blog')
+
+@endsection
