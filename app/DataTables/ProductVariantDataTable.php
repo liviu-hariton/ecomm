3b8ddd6ad6e215428b9d2fc865a718fda260069a
Liviu Hariton <liviu.hariton@gmail.com>
2023-10-26 14:23:01 +0300

    [Backend] Product variants

---
+++ app/DataTables/ProductVariantDataTable.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\ProductVariant;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class ProductVariantDataTable extends DataTable
+{
+    /**
+     * Build the DataTable class.
+     *
+     * @param QueryBuilder $query Results from query() method.
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return (new EloquentDataTable($query))
+            ->addColumn('action', function($query) {
+                $buttons = [
+                    'variant_items' => '<a href="#" class="btn btn-info btn-sm"><i class="fa fa-pencil-alt"></i> Variant items</a>',
+                    'edit' => '<a href="'.route('admin.variant.edit', $query).'" class="btn btn-warning btn-sm ml-1"><i class="fa fa-pencil-alt"></i></a>',
+                    'delete' => '<a href="'.route('admin.variant.destroy', $query).'" class="btn btn-danger btn-sm ml-1 delete-item" data-table="productvariant-table"><i class="fa fa-trash"></i></a>'
+                ];
+
+                return implode('', $buttons);
+            })
+            ->addColumn('vendor', function($query) {
+                return $query->product->vendor->user->name;
+            })
+            ->addColumn('variant name', function($query) {
+                return $query->name;
+            })
+            ->addColumn('active', function($query) {
+                return '<label class="custom-switch mt-2">
+                        <input type="checkbox" id="status-'.$query->id.'" value="'.$query->status.'" '.($query->status === 1 ? 'checked' : '').' data-id="'.$query->id.'" data-model="App^Models^ProductVariant" class="custom-switch-input change-status">
+                        <span class="custom-switch-indicator"></span>
+                      </label>';
+            })
+            ->rawColumns(['action', 'active'])
+            ->setRowId('id');
+    }
+
+    /**
+     * Get the query source of dataTable.
+     */
+    public function query(ProductVariant $model): QueryBuilder
+    {
+        $base_query = $model->newQuery()->with('product')->where('product_id', request('pid'));
+
+        return $base_query;
+    }
+
+    /**
+     * Optional method if you want to use the html builder.
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+                    ->setTableId('productvariant-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    //->dom('Bfrtip')
+                    ->orderBy(0, 'desc')
+                    ->selectStyleSingle()
+                    ->buttons([
+                        Button::make('excel'),
+                        Button::make('csv'),
+                        Button::make('pdf'),
+                        Button::make('print'),
+                        Button::make('reset'),
+                        Button::make('reload')
+                    ]);
+    }
+
+    /**
+     * Get the dataTable columns definition.
+     */
+    public function getColumns(): array
+    {
+        return [
+            Column::make('id')->width(50)->addClass('align-middle'),
+            Column::make('vendor')->addClass('align-middle'),
+            Column::make('variant name')->addClass('align-middle'),
+            Column::make('active')->addClass('align-middle text-center'),
+
+            Column::computed('action')
+                ->exportable(false)
+                ->printable(false)
+                ->width(200)
+                ->addClass('align-middle text-center'),
+        ];
+    }
+
+    /**
+     * Get the filename for export.
+     */
+    protected function filename(): string
+    {
+        return 'ProductVariant_' . date('YmdHis');
+    }
+}
---
+++ app/Http/Controllers/Backend/ProductVariantController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\DataTables\ProductVariantDataTable;
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\ProductVariant;
+use Illuminate\Http\Request;
+
+class ProductVariantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(ProductVariantDataTable $dataTable)
+    {
+        return $dataTable->render('admin.product.variant.index', [
+            'product' => Product::findOrFail(\request()->pid),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Request $request)
+    {
+        return view('admin.product.variant.create', [
+            'product' => Product::findOrFail(\request()->pid)
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated_data = $request->validate([
+            'product_id' => 'required|integer',
+            'name' => 'required|max:200',
+            'status' => 'required|integer'
+        ]);
+
+        $validated_data['vendor_id'] = auth()->user()->vendor->id;
+
+        ProductVariant::create($validated_data);
+
+        toastr('Product variant created successfully');
+
+        return redirect()->route('admin.variant.index', [
+            'pid' => $request->product_id
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProductVariant $variant)
+    {
+        return view('admin.product.variant.edit', [
+            'variant' => $variant
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProductVariant $variant)
+    {
+        $validated_data = $request->validate([
+            'name' => 'required|max:200',
+            'status' => 'required|integer'
+        ]);
+
+        $variant->update($validated_data);
+
+        toastr('Product variant updated successfully');
+
+        return redirect()->route('admin.variant.edit', $variant);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductVariant $variant)
+    {
+        $pid = $variant->product_id;
+
+        $variant->delete();
+
+        if(\request()->ajax()) {
+            return response([
+                'status' => 'success',
+                'message' => 'Item deleted successfully!'
+            ]);
+        } else {
+            toastr('Item deleted successfully!');
+
+            return redirect()->route('admin.variant.index', [
+                'pid' => $pid
+            ]);
+        }
+    }
+}
